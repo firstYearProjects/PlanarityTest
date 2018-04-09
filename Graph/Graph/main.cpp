4c6ab4b8890b49cd354915ea7813a1d6fb8169ff
Sergei Yarema <isara.isara8@gmail.com>
2018-04-09 20:08:33 +0300

    pl

--- Graph/Graph/main.cpp
+++ Graph/Graph/main.cpp
@@ -71,7 +71,33 @@ int main()
 		r.addEdge(2, 4);
 		r.addEdge(2, 5);
 
-		Test(r, "K33", 1);
+		Test(r, "almost K33", 1);
+	}
+
+	{
+		// K5 with odd
+		Graph g(8);
+		g.addEdge(0, 1);
+		g.addEdge(0, 2);
+		g.addEdge(0, 3);
+		g.addEdge(0, 4);
+
+		g.addEdge(1, 2);
+		g.addEdge(1, 3);
+		g.addEdge(1, 4);
+
+		g.addEdge(2, 5);
+		g.addEdge(5, 3);
+		g.addEdge(2, 4);
+
+		g.addEdge(3, 4);
+
+		g.addEdge(5, 6);
+		g.addEdge(6, 2);
+		//g.addEdge(5, 7);
+		//g.addEdge(6, 7);
+
+		Test(g, "K5 with odd", 0);
 	}
 
 
